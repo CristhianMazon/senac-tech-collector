d5c062135a67ef5d465073c918b14d04efd25a88
CristhianMazon <cristhian.s.mazon@gmail.com>
2025-08-22 19:53:49 -0300

    fundo corrigido

--- frontend/src/phaser/GameScene.js
+++ frontend/src/phaser/GameScene.js
@@ -82,7 +82,7 @@ export default class GameScene extends Phaser.Scene {
 
   preload() {
     // Carregar a nova imagem de fundo
-    this.load.image('fundoJogo', 'assets/FundoJogo.jpg');
+    this.load.image('fundoJogo', 'assets/FundoJogo.png');
 
     this.load.image('computador', 'assets/MonitorSenac.png');
     this.load.image('mouse', 'assets/MouseSenac.png');
