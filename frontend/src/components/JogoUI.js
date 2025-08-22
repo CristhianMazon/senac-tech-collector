54d0f734b05786f859f041dfac5b677376bb9aac
CristhianMazon <cristhian.s.mazon@gmail.com>
2025-08-22 20:26:32 -0300

    upgrades

--- frontend/src/components/JogoUI.js
+++ frontend/src/components/JogoUI.js
@@ -1,20 +1,20 @@
 import React from 'react';
+import './JogoUI.css';
 
-// Este componente é o HUD (Heads-Up Display) que fica sobre o jogo
 function JogoUI({ score, time, playerName }) {
-  return (
-    <>
-      <div id="info-usuario" className="game-ui">
-        Jogador: <span>{playerName}</span>
-      </div>
-      <div id="score-display" className="game-ui">
-        Pontos: <span>{score}</span>
-      </div>
-      <div id="timer-display" className="game-ui">
-        Tempo: <span>{time}</span>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div id="score-display" className="game-ui">
+                Pontos: {score}
+            </div>
+            <div id="timer-display" className="game-ui">
+                Tempo: {time}
+            </div>
+            <div id="info-usuario" className="game-ui">
+                {playerName}
+            </div>
+        </>
+    );
 }
 
 export default JogoUI;
